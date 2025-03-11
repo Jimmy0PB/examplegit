82e9caff5eaf228ff431bc46387b5154981234da
Alexander <alexander22piedrahita@gmail.com>
2025-03-10 21:49:40 -0400

    Segundo commit: Agregue un hola git

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!"); //Mi primer hola mundo
+        System.out.println("Hola git"); //Mi primer git
     }
 }
